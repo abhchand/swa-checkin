e131932046ec4c2800f6fab084b20b2bea153c3f
Abhishek Chandrasekhar <me@abhchand.me>
2019-02-04 22:34:09 -0500

    Using Capybara

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source "https://rubygems.org"
 ruby "2.5.1"
 
 gem "dotenv", "~> 2.6"
-gem "selenium-webdriver", "~> 3.141"
+gem "capybara", "~> 3.13", ">= 3.13.2"
--- swa.rb
+++ swa.rb
@@ -9,8 +9,9 @@
 #   \___| \___/  \__,_|  |__|  |__|__|  \_/\_/  |_____| \___|  |__|
 
 require "dotenv/load"
-require "selenium-webdriver"
+require "capybara"
 require "shellwords"
+require "logger"
 
 class SouthwestCheckInTask
   attr_accessor :logger, :driver, :config
@@ -32,32 +33,29 @@ class SouthwestCheckInTask
       visit("https://southwest.com")
 
       logger.debug("Clicking Check-In Tab")
-      checkin_el = driver.find_element(id: "TabbedArea_4-tab-4")
+      checkin_el = driver.find(id: "TabbedArea_4-tab-4")
       checkin_el.click
-      wait_for_ajax
 
       logger.debug("Fill out flight info")
 
-      confirmation_field = driver.find_element(id: "LandingPageAirReservationForm_confirmationNumber_check-in")
-      fname_field = driver.find_element(id: "LandingPageAirReservationForm_passengerFirstName_check-in")
-      lname_field = driver.find_element(id: "LandingPageAirReservationForm_passengerLastName_check-in")
-      submit = driver.find_element(id: "LandingPageAirReservationForm_submit-button_check-in")
-
+      confirmation_field = driver.find(id: "LandingPageAirReservationForm_confirmationNumber_check-in")
       confirmation_field.send_keys(confirmation)
-      fname_field.send_keys(fname)
-      lname_field.send_keys(lname)
-      submit.click
-      wait_for_ajax
 
-      sleep 3.0
+      fname_field = driver.find(id: "LandingPageAirReservationForm_passengerFirstName_check-in")
+      fname_field.send_keys(fname)
+
+      lname_field = driver.find(id: "LandingPageAirReservationForm_passengerLastName_check-in")
+      lname_field.send_keys(lname)
+
+      submit = driver.find(id: "LandingPageAirReservationForm_submit-button_check-in")
+      submit.click
 
       raise "Southwest Application Error" if page_has_error?
 
       # There's no id for this button element, but for now it's the only
       # `submit-button` class on the page. Fingers crossed it stays that way.
-      submit = driver.find_element(:class, "form-mixin--submit-button")
+      submit = driver.find(:css, ".form-mixin--submit-button")
       submit.click
-      wait_for_ajax
 
       sleep 3.0
 
@@ -134,23 +132,20 @@ class SouthwestCheckInTask
   def setup_logger
     @logger_filepath = "/tmp/swa-#{Time.now.utc.to_i}.log"
     puts "Logging to #{@logger_filepath}"
-    @logger = Logger.new(@logger_filepath)
+    @logger = ::Logger.new(@logger_filepath)
     @logger.info("Logging enabled")
   end
 
   def setup_driver
     logger.debug("Creating driver")
-
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    @driver = Selenium::WebDriver.for(:chrome, options: options)
+    @driver = Capybara::Session.new(:selenium_chrome_headless)
   end
 
   def setup_headless_window
     width = 1600
     height = 1280
     logger.debug("Resizing window to #{width}x#{height}")
-    driver.manage.window.resize_to(width, height)
+    driver.current_window.resize_to(width, height)
   end
 
   # Executes a block and ensures the headless browser connection is closed
@@ -162,6 +157,8 @@ class SouthwestCheckInTask
   rescue => e
     logger.error e
     logger.error e.backtrace
+    logger.debug "Page Body:"
+    logger.debug driver.body
   ensure
     capture_screenshot!
 
@@ -175,24 +172,26 @@ class SouthwestCheckInTask
 
   def visit(url, log: true)
     logger.debug("Visiting #{url}") if log
-    driver.navigate.to(url)
-    wait_for_ajax
+    driver.visit(url)
   end
 
   def page_has_error?
     # In several cases the Southwest website returns a page with an
     # error flash/div at the top. This occurs when the check in doesn't exist,
     # is too early, has already passed, etc...
-    driver.find_elements(class: "message_error").any?
+    driver.find(:css, ".message_error")
+    true
+  rescue Capybara::ElementNotFound => e
+    false
   end
 
   def capture_screenshot!
-    @screenshot_filename = "screenshot-#{Time.now.utc.to_i}.png"
-    @screenshot_filepath = File.join("/tmp", @screenshot_filename)
+    @screenshot_filename = "/tmp/screenshot-#{Time.now.utc.to_i}.png"
 
-    logger.debug "Capturing screenshot... #{@screenshot_filepath}"
-    puts "Capturing screenshot... #{@screenshot_filepath}"
-    driver.save_screenshot(@screenshot_filepath) if driver
+    logger.debug "Capturing screenshot... #{@screenshot_filename}"
+    puts "Capturing screenshot... #{@screenshot_filename}"
+
+    driver.save_screenshot(@screenshot_filename) if driver
   end
 
   def send_mail?
@@ -216,7 +215,7 @@ class SouthwestCheckInTask
       "-s", email_server,
       "-xu", email_sender,
       "-xp", escape(email_password),
-      "-a", @screenshot_filepath
+      "-a", @screenshot_filename
     ].join(" ")
 
     logger.debug("sendemail command: '#{cmd}'")
@@ -227,15 +226,8 @@ class SouthwestCheckInTask
     Shellwords.escape(str)
   end
 
-  def wait_for_ajax
-    max_time = Time.now + 10
-
-    while Time.now < max_time
-      finished = driver.execute_script('$.active').to_i == 0
-      break if finished
-      sleep 0.1
-    end
-    raise "wait_for_ajax timeout" unless finished
+  def page
+    Capybara.current_session
   end
 end
 
